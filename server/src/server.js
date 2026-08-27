a7ace51907a876fb151e92e8cdcdeea2fc29a7c4
Tanisha Ali <tanishaali89@gmail.com>
2026-08-27 23:49:23 +0530

    Connect MongoDB, Connect React → Express

--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,12 @@
 import dotenv from "dotenv";
 import app from "./app.js";
+import connectDB from "./config/db.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
+
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`PlacementOZ server running on port ${PORT}`);
